514ad5d051dad761283b436819ee2ee7a6b14a01
KominP <komin.94@mail.ru>
2024-11-01 01:21:33 +0500

    add const alert console.log

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "LG";
+const screens = 3;
+const screenPrise = 100;
+const rollback = 10;
+const fullPrise = 110;
+const adaptive = true;
+
+alert(title);
+
+console.log(title.length);
