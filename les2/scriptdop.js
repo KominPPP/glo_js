441b1dc886eeab05bcd63d1a0b62b0c9a78feb3e
KominP <komin.94@mail.ru>
2024-11-06 01:37:44 +0500

    les03 task complete

--- les2/scriptdop.js
+++
@@ -1,3 +0,0 @@
-const num = 266219;
-
-console.log(num[0] * num[1] * num[2] * num[3] * num[4] * num[4]);
--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let title = promt("Как называется ваш проект?");
-let screens = promt("Выберите тип экрана: простой, сложный, интерактивный!");
-let screenPrice = +promt("Введите стоимость экрана");
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Выберите тип экрана: простой, сложный, интерактивный!");
+let screenPrice = +prompt("Введите стоимость экрана");
 let adaptive = confirm("Нужен ли адаптив эрана?");
-let service1 = promt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +promt("Введите стоимость услуги");
-let service2 = promt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +promt("Введите стоимость услуги");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Введите стоимость услуги");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Введите стоимость услуги");
 
 let rollback = 10;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
-console.log(servicePercentPrice);
+console.log("Стоимость верстки сайта " + fullPrice + " руб.");
+console.log(
+  "Стоимость верстки сайта с затратами " + servicePercentPrice + " руб."
+);
+
+if (fullPrice >= 3000) {
+  console.log("Даем скидку 10%");
+} else if (fullPrice >= 1500 && fullPrice < 3000) {
+  console.log("Даем скидку 10%");
+} else if (fullPrice < 1500 && fullPrice >= 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так");
+}
 
 // console.log(typeof title, typeof fullPrice, typeof adaptive);
 // console.log(screens.length);
