7152978463a6ffe46d75810c8f3edb5dbf837700
KominP <komin.94@mail.ru>
2024-11-06 01:10:47 +0500

    lesson03 task

---
+++ les2/scriptdop.js
@@ -0,0 +1,3 @@
+const num = 266219;
+
+console.log(num[0] * num[1] * num[2] * num[3] * num[4] * num[4]);
--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-let title = "DevLab";
-let screens = "Simple, Hard, Interactive";
-const screenPrice = 50;
+let title = promt("Как называется ваш проект?");
+let screens = promt("Выберите тип экрана: простой, сложный, интерактивный!");
+let screenPrice = +promt("Введите стоимость экрана");
+let adaptive = confirm("Нужен ли адаптив эрана?");
+let service1 = promt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +promt("Введите стоимость услуги");
+let service2 = promt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +promt("Введите стоимость услуги");
+
 let rollback = 10;
-let fullPrice = 150;
-let adaptive = true;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+console.log(servicePercentPrice);
 
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов " + screenPrice + " $");
-console.log("Стоимость верстки сайта " + fullPrice + " $");
+// console.log(typeof title, typeof fullPrice, typeof adaptive);
+// console.log(screens.length);
+// console.log("Стоимость верстки экранов " + screenPrice + " $");
+// console.log("Стоимость верстки сайта " + fullPrice + " $");
 
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100) + " $");
+// console.log(screens.toLowerCase().split(", "));
+// console.log(fullPrice * (rollback / 100) + " $");
