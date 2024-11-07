a023b248d7ecd08a49f21e0ddd5f71c3bd35d5b6
KominP <komin.94@mail.ru>
2024-11-07 23:20:41 +0500

    lesson4 task

--- script.js
+++ script.js
@@ -1,34 +1,72 @@
+"use strict";
+
 let title = prompt("Как называется ваш проект?");
-let screens = prompt("Выберите тип экрана: простой, сложный, интерактивный!");
-let screenPrice = +prompt("Введите стоимость экрана");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Введите стоимость данной работы");
 let adaptive = confirm("Нужен ли адаптив эрана?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Введите стоимость услуги");
+let servicePrice1 = +prompt("Введите стоимость доп. услуги");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Введите стоимость услуги");
-
+let servicePrice2 = +prompt("Введите стоимость доп. услуги");
 let rollback = 10;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
-console.log("Стоимость верстки сайта " + fullPrice + " руб.");
-console.log(
-  "Стоимость верстки сайта с затратами " + servicePercentPrice + " руб."
-);
+let fullPrice;
+let servicePercentPrice;
+let allServicePrice;
 
-if (fullPrice >= 3000) {
-  console.log("Даем скидку 10%");
-} else if (fullPrice >= 1500 && fullPrice < 3000) {
-  console.log("Даем скидку 10%");
-} else if (fullPrice < 1500 && fullPrice >= 0) {
-  console.log("Скидка не предусмотрена");
-} else {
-  console.log("Что-то пошло не так");
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+
+const getRollbackMassage = function (price) {
+  if (price >= 30000) {
+    return "Даем скидку 10%";
+  } else if (price >= 15000 && price < 30000) {
+    return "Даем скидку 10%";
+  } else if (price < 15000 && price >= 0) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что-то пошло не так";
+  }
+};
+
+const getAllServicePrice = function () {
+  return servicePrice1 + servicePrice2;
+};
+allServicePrice = getAllServicePrice();
+
+function getFullPrice() {
+  fullPrice = screenPrice + allServicePrice;
+  return fullPrice;
 }
 
-// console.log(typeof title, typeof fullPrice, typeof adaptive);
-// console.log(screens.length);
-// console.log("Стоимость верстки экранов " + screenPrice + " $");
-// console.log("Стоимость верстки сайта " + fullPrice + " $");
+const getTitle = function (str) {
+  str = str.trimStart();
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+};
 
-// console.log(screens.toLowerCase().split(", "));
-// console.log(fullPrice * (rollback / 100) + " $");
+const getServicePercentPrice = function () {
+  servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+  return servicePercentPrice;
+};
+
+showTypeOf(title);
+showTypeOf(adaptive);
+showTypeOf(screenPrice);
+getFullPrice();
+getServicePercentPrice();
+
+console.log(getTitle(title));
+console.log(getRollbackMassage(fullPrice));
+
+console.log(screens.length);
+
+console.log(servicePercentPrice);
+console.log(
+  "Стоимость верстки сайта " +
+    screenPrice +
+    " ₽ и Стоимость разработки сайта " +
+    fullPrice +
+    " ₽"
+);
+
+console.log("(доп.услуги " + allServicePrice + " ₽)");
