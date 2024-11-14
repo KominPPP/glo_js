77db3cca56a8df59cebb9a68f5882571a1186d9b
KominP <komin.94@mail.ru>
2024-11-15 00:20:10 +0500

    les08 dop

---
+++ js/script.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const appData = {
+  title: "",
+  screens: [],
+  screenPrice: 0,
+  adaptive: true,
+  rollback: 10,
+  fullPrice: 0,
+  servicePercentPrice: 0,
+  allServicePrice: 0,
+  services: {},
+
+  start: function () {
+    appData.asking();
+    appData.addPrices();
+    appData.getFullPrice();
+    appData.getServicePercentPrice();
+    appData.getTitle();
+    appData.logger();
+  },
+
+  isNumber: function (num) {
+    return !isNaN(parseFloat(num) && isFinite(num));
+  },
+
+  isString: function (str) {
+    return (
+      (typeof str === "string" && str.trim() !== "") || str instanceof String
+    );
+  },
+
+  asking: function () {
+    appData.title = prompt("Как называется ваш проект?");
+
+    for (let i = 0; i < 2; i++) {
+      let name = prompt("Какие типы экранов нужно разработать?");
+      let price = 0;
+
+      do {
+        price = prompt("Сколько будет стоить данная работа");
+      } while (!appData.isNumber(price));
+
+      appData.screens.push({ id: i, name: name, price: price });
+    }
+
+    for (let i = 0; i < 2; i++) {
+      let name = prompt("Какой дополнительный тип услуги нужен?");
+      let price = 0;
+      do {
+        price = prompt("Сколько будет стоить доп.услуга");
+      } while (!appData.isNumber(price));
+
+      appData.services[name] = +price;
+    }
+
+    appData.adaptive = confirm("Нужен ли адаптив на сайте?");
+  },
+
+  addPrices: function () {
+    for (let screen of appData.screens) {
+      appData.screenPrice += +screen.price;
+    }
+
+    for (let key in appData.services) {
+      appData.allServicePrice += appData.services[key];
+    }
+  },
+
+  getFullPrice: function () {
+    appData.fullPrice = +appData.screenPrice + appData.allServicePrice;
+  },
+
+  getServicePercentPrice: function () {
+    appData.servicePercentPrice =
+      appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+  },
+
+  getTitle: function () {
+    appData.title =
+      appData.title.trim()[0].toUpperCase() +
+      appData.title.trim().slice(1).toLocaleLowerCase();
+  },
+
+  getRollbackMassage: function (price) {
+    if (price >= 30000) {
+      return "Даем скидку 10%";
+    } else if (price >= 15000 && price < 30000) {
+      return "Даем скидку 5%";
+    } else if (price < 15000 && price >= 0) {
+      return "Скидка не предусмотрена";
+    } else {
+      return "Что-то пошло не так";
+    }
+  },
+
+  logger: function () {
+    console.log(appData.fullPrice);
+    console.log(appData.servicePercentPrice);
+    console.log(appData.screens);
+  },
+};
+
+appData.start();
